c23628d82567d0b7e25cb8b52c671a4be40d6fff
Timo Schmidt <timo.hund@yahoo.com>
2018-03-13 13:45:20 +0100

    Check travis output

--- app/configuration/AdditionalConfiguration.php
+++ app/configuration/AdditionalConfiguration.php
@@ -1,5 +1,5 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = 'localhost:8000|app:8000';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = '127.0.0.1:8000|localhost:8000|app:8000';
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['sites'][1]['domains'][]  = 'localhost';
