e89efa3930beeb2f1d0d693a3f2fa389f46d3863
Timo Schmidt <timo.hund@yahoo.com>
2018-03-13 13:34:51 +0100

    Improved site configuration

--- app/configuration/AdditionalConfiguration.php
+++ app/configuration/AdditionalConfiguration.php
@@ -1,3 +1,5 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = '.*';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = 'localhost:8000|app:8000';
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['sites'][1]['domains'][]  = 'localhost';
