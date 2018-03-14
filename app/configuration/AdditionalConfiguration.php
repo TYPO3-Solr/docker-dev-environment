5d460b267447a2d61f5b8d30b85d941503cbdcc5
Timo Schmidt <timo.hund@yahoo.com>
2018-03-14 10:19:00 +0100

    [TASK] Add default tika configuration

--- app/configuration/AdditionalConfiguration.php
+++ app/configuration/AdditionalConfiguration.php
@@ -3,3 +3,5 @@
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern']  = 'localhost|local.typo3.org';
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['sites'][1]['domains'][]  = 'local.typo3.org';
+
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['tika'] = 'a:14:{s:9:"extractor";s:6:"server";s:7:"logging";s:1:"0";s:18:"showTikaSolrModule";s:1:"1";s:16:"excludeMimeTypes";s:0:"";s:13:"fileSizeLimit";s:3:"500";s:8:"tikaPath";s:0:"";s:14:"tikaServerPath";s:0:"";s:16:"tikaServerScheme";s:4:"http";s:14:"tikaServerHost";s:4:"tika";s:14:"tikaServerPort";s:4:"9998";s:10:"solrScheme";s:4:"http";s:8:"solrHost";s:9:"localhost";s:8:"solrPort";s:4:"8080";s:8:"solrPath";s:6:"/solr/";}';
